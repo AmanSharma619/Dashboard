4fa4ef8bd14bfc04bc0c15f2cfe1e47d3e563ea0
aman sharma <amansharma.as536@gmail.com>
2025-07-12 16:07:01 +0530

    initial commit + login page design

---
+++ src/pages/Login.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import { LayoutDashboard } from 'lucide-react';
+const Login = () => {
+    return (
+        <div className='w-screen h-screen flex bg-gray-200 items-center justify-center'>
+            <div className="container h-[90%] w-[90%] bg-white rounded-4xl flex p-2 border-0 border-fuchsia-500">
+                <div className="left w-1/2 flex flex-col justify-center items-center">
+                    <div className="left-subdiv w-[85%] h-[85%]  flex flex-col justify-evenly">
+                        <div>
+                        <LayoutDashboard size={28} color="#9211e8" strokeWidth={2.75} />
+                        <h1 className='font-bold '>Dashboard</h1>
+                        </div>
+                        <div className='w-full h-[70%] flex flex-col justify-between '>
+                            <span >
+                                <h1 className='text-5xl font-bold '>Hello,</h1>
+                                <h1 className='text-5xl font-bold '>Welcome Back</h1>
+                                <h2 className='mt-2'>Hey, Welcome back to your special place</h2>
+                            </span>
+                            <div className="formdiv w-full">
+                                <form className='flex flex-col gap-4' >
+                                    <input type="text" className=' p-1 w-[50%] h-[40px] rounded-lg border-2 border-gray-400' placeholder='Email' />
+                                    <input type="password" className='p-1 w-[50%] h-[40px] rounded-lg border-2 border-gray-400' placeholder='Password' />
+                                    <button className='text-xl bg-purple-600 w-[100px] text-white font-bold p-2 rounded-lg'>Sign In</button>
+                                </form>
+                            </div>
+                            <div className='text-gray-700 flex gap-1'>
+                                <h2>Don't have an account?</h2>
+                              <h2 className="font-bold bg-gradient-to-r from-violet-500 to-fuchsia-500 bg-clip-text text-transparent">Sign Up</h2>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="right w-1/2  relative">
+                    <img src="/loginImage.webp" className='w-full h-full object-cover rounded-4xl' alt="" />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Login
